58c0d7d576dbbdf917c9034299db2f101560c7e1
jesse-hub <jesse_earth@outlook.com>
2014-05-09 14:14:26 +0800

    Initial commit

---
+++ files/code/octopress-add-original-link-v2.rb
@@ -0,0 +1,8 @@
+
+<p class='post-footer' style="font-size:70%;" >
+<br/>
+<br/>
+<br/>
+#{pre or "The Original Link:"}[#{post.full_url}](#{post.url})<br/>
+If you want to reprint it, please do under the  <a rel="nofollow" href=#{license}>#{lic_name}</a>
+</p>
