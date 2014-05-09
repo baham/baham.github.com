d5b5d78a29023b90a9813f21170aecb25f47449b
jesse-hub <jesse_earth@outlook.com>
2014-05-09 14:30:28 +0800

    deleted:    .directory

--- public/files/code/add_keywords_and_description.rb
+++
@@ -1,4 +0,0 @@
-	{% capture description %}{% if page.description %}{{ page.description }}{% elsif site.description %}{{ site.description }}{% else %}{{ content | raw_content }}{% endif %}{% endcapture %}
-	<meta name="description" content="{{ description | strip_html | condense_spaces | truncate:150 }}" />
-	{% capture keywords %}{% if page.keywords %}{{ page.keywords }}{% elsif site.keywords %}{{ site.keywords }}{% endif %}{% endcapture %}
-	<meta name="keywords" content="{{ keywords | strip_html | condense_spaces }}" />
