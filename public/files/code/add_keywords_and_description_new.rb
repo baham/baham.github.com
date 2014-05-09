778a4a46d7706b7c7580d39806b41b6cec60b80f
jesse-hub <jesse_earth@outlook.com>
2014-05-09 14:37:14 +0800

    new blog content

--- public/files/code/add_keywords_and_description_new.rb
+++
@@ -1,5 +0,0 @@
-{% capture description %}{% if page.description %}{{ page.description }}{% elsif page.title %}{{ content | raw_content }}{% else %}{{ site.description }}{% endif %}{% endcapture %}
-<meta name="description" content="{{ description | strip_html | condense_spaces | truncate:150 }}" />
-
-{% capture keywords %}{% if page.keywords %}{{ page.keywords }}{% elsif page.title %}{{""}}{% else %}{{ site.keywords }}{% endif %}{% endcapture %}
-<meta name="keywords" content="{{ keywords | strip_html | condense_spaces }}" />
